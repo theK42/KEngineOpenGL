bf7ea7d312357daf0adb41ad2ecd7fd4327ae69a
Kelson <theK42@gmail.com>
2022-11-05 22:18:23 -0700

    Fix for GLES 3.0 (WebGL) compatibility.

--- TextRenderer.cpp
+++ TextRenderer.cpp
@@ -30,8 +30,8 @@ void KEngineOpenGL::TextSprite::Init(int width, int height, const KEngineOpenGL:
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); 
-	
-	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, mSprite.texture, 0);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mSprite.texture, 0);
 
 	GLenum drawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
 	glDrawBuffers(1, drawBuffers); 
